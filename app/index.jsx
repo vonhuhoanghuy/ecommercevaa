580c43a9c58fc77d37043bfe5afc47f81bf82663
unknown <vonhuhoanghuy@gmail.com>
2025-01-11 13:39:45 +0700

    first

---
+++ app/index.jsx
@@ -0,0 +1,15 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+
+export default function index() {
+  return (
+    <SafeAreaView>
+        <View>
+            <Text >hello world cu hao 123</Text>
+        </View>
+    </SafeAreaView>
+    
+  )
+}
+
